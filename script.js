91beb7956b2f68fab927d8f68e10e7b3e41bb61b
DonQuixuote <donquixuote@gmail.com>
2025-02-03 13:07:29 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -513,10 +513,9 @@ function showElementDetails(elementSymbol, elementColor) {
     // Add GIF for any element
     const gifContent = `
         <div class="element-gif">
-            <img src="https://raw.githubusercontent.com/Donquixuote/Otomology-Bank/gh-pages/gifs/${elementSymbol}.gif"
+            <img src="gifs/${elementSymbol}.gif" 
                  alt="Element ${elementSymbol} animation" 
-                 onerror="console.log('Failed to load GIF:', this.src); this.parentElement.style.display='none'"
-                 onload="console.log('Successfully loaded:', this.src)">
+                 onerror="this.parentElement.style.display='none'">
         </div>
     `;
 
